e4eacb3008ef877cf273177b428a09f4e6196909
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-09-21 11:39:48 +0100

    product search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,22 +1,35 @@
+import { Product } from '../../models/app-user';
 import { ProductService } from '../../product/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-
-  product$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(
     private productService: ProductService
   ) {
-    this.product$ = this.productService.getAll();
-   }
+    this.subscription = this.productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ? this.products.filter(d => {
+      return d.title.toLowerCase().includes(query.toLowerCase()) || d.description.toLowerCase().includes(query.toLowerCase());
+    }) : this.products;
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
