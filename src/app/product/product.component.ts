ddf859575b76e7da71f212fc98c3198c7a29810f
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-10-03 11:01:47 +0100

    product list

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,9 @@
+import { Product } from './../models/app-user';
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from '../category/category.service';
 import { ProductService } from './product.service';
 import { Component } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-product',
@@ -8,14 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  products$;
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
   categories$;
+  category: string;
 
   constructor(
+    route: ActivatedRoute,
     productService: ProductService,
     categoryService: CategoryService
   ) {
-    this.products$ = productService.getAll();
+
+    productService.getAll().switchMap(products => {
+      this.products = products;
+      return route.queryParamMap;
+    })
+    .subscribe(params => {
+        this.category = params.get('category');
+
+        this.filteredProducts = (this.category) ?
+          this.products.filter(p => p.category === this.category) :
+          this.products;
+    });
+
     this.categories$ = categoryService.getAll();
   }
 
