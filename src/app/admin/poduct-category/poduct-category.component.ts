8b499a833ae89f43106019e6ee1db7504d0514eb
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-11-09 11:05:22 +0100

    improved product card layout, improved admin category

--- src/app/admin/poduct-category/poduct-category.component.ts
+++ src/app/admin/poduct-category/poduct-category.component.ts
@@ -2,6 +2,7 @@ import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../../category/category.service';
 import { Component, Input } from '@angular/core';
 import { ICategory } from '../../models/models';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-poduct-category',
@@ -12,15 +13,36 @@ export class PoductCategoryComponent {
 
   categories$;
   categoryParams: any = {};
+  closeResult: string;
 
   constructor(
     categoryService: CategoryService,
     private route: ActivatedRoute,
+    private modalService: NgbModal
   ) {
     this.categories$ = categoryService.getAllCategories();
     this.route.queryParams.subscribe(params => {
       this.categoryParams = params;
     });
+  }
+
+  open(content) {
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      console.log(reason);
+      return  `with: ${reason}`;
+    }
   }
 
 }
