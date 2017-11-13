2fe12db258978b0b63eb627eee01bddf9ebe3aed
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2017-11-13 19:45:27 +0100

    slide show, call to action

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgxCarousel } from 'ngx-carousel';
 
 @Component({
@@ -6,26 +6,20 @@ import { NgxCarousel } from 'ngx-carousel';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
-  public carouselOne: NgxCarousel;
-
-  constructor() { }
-
-  ngOnInit() {
-    this.carouselOne = {
-      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
-      slide: 1,
-      speed: 400,
-      interval: 4000,
-      point: {
-        visible: true
-      },
-      load: 2,
-      touch: true,
-      loop: true,
-      custom: 'banner'
-    };
-  }
+export class SlideshowComponent {
+  public carouselOne: NgxCarousel = {
+    grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+    slide: 1,
+    speed: 400,
+    interval: 4000,
+    point: {
+      visible: true
+    },
+    load: 2,
+    touch: true,
+    loop: true,
+    custom: 'banner'
+  };
 
   loader(event) {
 
